46f84d32874db699fee50bed5195db3aba0f7a39
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2021-01-21 15:05:58 +0100

    intro scene, genreate random val and main scene loader

---
+++ Assets/MyGame/Scripts/ShowRandomValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowRandomValue : MonoBehaviour
+{
+
+    public Text guiTextRandomVal;
+    public MyScriptableObject myData;
+
+    public void GetRandomVal()
+    {
+        float rndVal = Random.Range(1.0f,10.0f);
+        guiTextRandomVal.text = rndVal.ToString();
+        myData.randomVal = rndVal;
+        myData.test = "Kilian";
+    }
+}
