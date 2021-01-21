7f20ebd7c75184079502ddb0e8a444c29ba3b962
Kilian Reisinger <kilian.reisinger02@htl-salzburg.ac.at>
2021-01-21 14:53:18 +0100

    Scriptable Object and load random val

---
+++ Assets/MyGame/Scripts/LoadRandomVal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadRandomVal : MonoBehaviour
+{
+    public MyScriptableObject myData;
+    public Text randomValText;
+   public void LoadRandomValue()
+    {
+        randomValText.text = myData.randomVal.ToString();
+    }
+}
